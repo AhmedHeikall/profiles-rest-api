2beaa2f27f7b88754b93dce935e7264a2a2d8317
Ahmed Heikal <aheikal424@icloud.com>
2022-11-20 00:13:11 +0200

    Create Login API

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,6 +1,9 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.authentication import TokenAuthentication
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
+
 from rest_framework import status, viewsets
 from rest_framework import filters
 
@@ -66,3 +69,9 @@ class UserProfileViewSet(viewsets.ModelViewSet):
         "name",
         "email",
     )
+
+
+class UserLoginApiView(ObtainAuthToken):
+    """Handle creating user authentication tokens"""
+
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
